71ff8e5aafdcb29272a1900319efc07abc3d8b2d
kevin-ZHB <hongbozhou0@gmail.com>
2022-08-07 00:02:06 +1000

    new material

--- Programming and Software Development/Hello.java
+++ Programming and Software Development/Hello.java
@@ -1,8 +1,8 @@
-// Hello.java
-public class Hello{
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        
+public class Hello {
+    public static void  main(String[] args){
+        int x = 10;
+        int y = 4;
+        System.out.println( (x % y ==2) || (++x == 10));
+        System.out.println((++x == 10));
     }
-
 }
